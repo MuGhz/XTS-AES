63315ff2b2b6ac087b71ca85907a18f7429b5c3b
galuhestya <galuhestya@gmail.com>
2018-04-20 20:09:25 +0700

    kelar, tapi menjijikan

--- Trial.java
+++ Trial.java
@@ -1,12 +1,16 @@
 public class Trial	{
 	public static void main(String[] args)	{
 		byte[] input = {
-			(byte)0x14, (byte)0x24
+			(byte)0x14, (byte)0x24,(byte)0x14, (byte)0x24,
+			(byte)0x14, (byte)0x24,(byte)0x14, (byte)0x24,
+			(byte)0x14, (byte)0x24,(byte)0x14, (byte)0x24,
+			(byte)0x14, (byte)0x24,(byte)0x14, (byte)0x24
 		};
-		Xts x = new Xts(input,"aassddffgghhjjkk");
-		System.out.println(x.getPlainText());
-		System.out.println(x.getKeyOne());
-		System.out.println(x.getKeyTwo());
-		System.out.println("DONE");
+		Xts x = new Xts(input,"AASSDDFFGGHHJJKKAASSDDFFGGHHJJKKAASSDDFFGGHHJJKKAASSDDFFGGHHJJKK");
+		System.out.println(Util.toHEX(input));
+		String enz = x.encode().replace(" ","");
+		System.out.println(enz);
+		Xts out = new Xts(Util.hexStringToByteArray(enz),"AASSDDFFGGHHJJKKAASSDDFFGGHHJJKKAASSDDFFGGHHJJKKAASSDDFFGGHHJJKK");
+		System.out.println(out.decode());
 	}
 }
--- Xts.java
+++ Xts.java
@@ -1,40 +1,118 @@
+import java.util.Arrays;
+
 public class Xts{
 
 	byte[] plaintext;
-	String key1;
-	String key2;
-	byte[] key1byte;
-	byte[] key2byte;
+	byte[] key;
 
 	public Xts(byte[] plaintext, String key)	{
 		this.plaintext = plaintext;
-		this.key1 = key.substring(0,8);
-		this.key2 = key.substring(8,16);
+		this.key = Util.hexStringToByteArray(key);
 	}
 
 	public byte[] getPlainText()	{
 		return this.plaintext;
 	}
 
-	public String getKeyOne()	{
-		return this.key1;
-	}
-
-	public String getKeyTwo()	{
-		return this.key2;
+	public byte[] getKey()	{
+		return this.key;
 	}
 
 	public String encode()	{
-		if(plaintext.length() % 2 == 0)	{
-			for(int i = 0; i < plaintext.length(); i += 16)	{
-
-			}	
+		int len = this.plaintext.length;
+		byte[] enc = new byte[len];
+		//fullblock
+		if(len % 16 == 0)	{
+			for(int i = 0; i < len; i += 16)	{
+				byte[] block = Arrays.copyOfRange(this.plaintext,i,i+16);
+				byte[] encrypted = XTSAES.encryptBlock(this.key,i,block);
+				for(int j = i; j < i+16; j++)	{
+					enc[j] = encrypted[j%16];
+				}
+			}
 		}
-		return "OK";
+		//handle ciphertext stealing
+		else	{
+			int diff = len%16;
+			byte[] last = new byte[len-diff];
+			for(int i = diff; i > -1; i--)	{
+				last[diff-i] = this.plaintext[len-i];
+			}
+			int d = 0;
+			for(int i = 0; i < len-diff; i += 16)	{
+				byte[] block = Arrays.copyOfRange(this.plaintext,i,i+16);
+				byte[] encrypted = XTSAES.encryptBlock(this.key,i,block);
+				if(i+16 > len)	{
+					int c = 0;
+					for(int j = len-diff; j < len; j++)	{
+						enc[j] = encrypted[c++];
+					}
+					for(int j = c; j < 16; j++)	{
+						last[j] = encrypted[j];
+					}
+					d = i+16;
+				} else	{
+					for(int j = i; j < i+16; j++)	{
+						enc[j] = encrypted[j%16];
+					}
+				}
+			}
+			byte[] final_block = XTSAES.encryptBlock(this.key,d,last);
+			int last_empty_block = len-diff-16;
+			for(int i = 0; i < 16; i++)	{
+				enc[last_empty_block+i] = final_block[i];
+			}
+		}
+		String result = Util.toHEX(enc);
+		return result;
 	}
 
 	public String decode()	{
-		
-		return "OK";
+		int len = this.plaintext.length;
+		byte[] enc = new byte[len];
+		//fullblock
+		if(len % 16 == 0)	{
+			for(int i = 0; i < len; i += 16)	{
+				byte[] block = Arrays.copyOfRange(this.plaintext,i,i+16);
+				byte[] encrypted = XTSAES.decryptBlock(this.key,i,block);
+				for(int j = i; j < i+16; j++)	{
+					enc[j] = encrypted[j%16];
+				}
+			}
+		}
+		//handle ciphertext stealing
+		else	{
+			int diff = len%16;
+			byte[] last = new byte[len-diff];
+			for(int i = diff; i > -1; i--)	{
+				last[diff-i] = this.plaintext[len-i];
+			}
+			int d = 0;
+			for(int i = 0; i < len-diff; i += 16)	{
+				byte[] block = Arrays.copyOfRange(this.plaintext,i,i+16);
+				byte[] encrypted = XTSAES.decryptBlock(this.key,i,block);
+				if(i+16 > len)	{
+					int c = 0;
+					for(int j = len-diff; j < len; j++)	{
+						enc[j] = encrypted[c++];
+					}
+					for(int j = c; j < 16; j++)	{
+						last[j] = encrypted[j];
+					}
+					d = i+16;
+				} else	{
+					for(int j = i; j < i+16; j++)	{
+						enc[j] = encrypted[j%16];
+					}
+				}
+			}
+			byte[] final_block = XTSAES.decryptBlock(this.key,d,last);
+			int last_empty_block = len-diff-16;
+			for(int i = 0; i < 16; i++)	{
+				enc[last_empty_block+i] = final_block[i];
+			}
+		}
+		String result = Util.toHEX(enc);
+		return result;
 	}
 }
