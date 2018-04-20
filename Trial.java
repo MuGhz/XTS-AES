dca9af54731ad77b405b478db799e3af64c26ea6
galuhestya <galuhestya@gmail.com>
2018-04-20 14:01:32 +0700

    tambah class buat nyoba input

--- Trial.java
+++ Trial.java
@@ -2,7 +2,10 @@ import Xts;
 
 public class Trial	{
 	public static void main(String[] args)	{
-		Xts x = new Xts("aaaaaaaaaaa","aassddffgghhjjkk");
+		byte[] = {
+			(byte)0x14, (byte)0x24
+		};
+		Xts x = new Xts(,"aassddffgghhjjkk");
 		System.out.println(x.getPlainText());
 		System.out.println(x.getKeyOne());
 		System.out.println(x.getKeyTwo());
