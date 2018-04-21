c1a1a3343626d6204c895ebe0f5af2da6d843135
galuhestya <galuhestya@gmail.com>
2018-04-21 17:39:24 +0700

    Asd

--- Xts.java
+++ Xts.java
@@ -25,7 +25,7 @@ public class Xts{
 		if(len % 16 == 0)	{
 			for(int i = 0; i < len; i += 16)	{
 				byte[] block = Arrays.copyOfRange(this.plaintext,i,i+16);
-				byte[] encrypted = XTSAES.encryptBlock(this.key,i/16,block);
+				byte[] encrypted = XTSAES.encryptBlock(this.key,0,block);
 				for(int j = i; j < i+16; j++)	{
 					enc[j] = encrypted[j%16];
 				}
@@ -41,7 +41,7 @@ public class Xts{
 			int d = 0;
 			for(int i = 0; i < len-diff; i += 16)	{
 				byte[] block = Arrays.copyOfRange(this.plaintext,i,i+16);
-				byte[] encrypted = XTSAES.encryptBlock(this.key,i/16,block);
+				byte[] encrypted = XTSAES.encryptBlock(this.key,0,block);
 				//if its 1 block before final block
 				if(i+16+diff >= len)	{
 					int c = 0;
@@ -60,7 +60,7 @@ public class Xts{
 					}
 				}
 			}
-			byte[] final_block = XTSAES.encryptBlock(this.key,d,last);
+			byte[] final_block = XTSAES.encryptBlock(this.key,0,last);
 			int last_empty_block = len-diff-16;
 			for(int i = 0; i < 16; i++)	{
 				enc[last_empty_block+i] = final_block[i];
@@ -76,7 +76,7 @@ public class Xts{
 		if(len % 16 == 0)	{
 			for(int i = 0; i < len; i += 16)	{
 				byte[] block = Arrays.copyOfRange(this.plaintext,i,i+16);
-				byte[] encrypted = XTSAES.decryptBlock(this.key,i/16,block);
+				byte[] encrypted = XTSAES.decryptBlock(this.key,0,block);
 				for(int j = i; j < i+16; j++)	{
 					dec[j] = encrypted[j%16];
 				}
@@ -92,8 +92,9 @@ public class Xts{
 			int d = 0;
 			for(int i = 0; i < len-diff; i += 16)	{
 				byte[] block = Arrays.copyOfRange(this.plaintext,i,i+16);
+				System.out.println(Util.toHEX(block));
 				if(i+16+diff >= len)	{
-					byte[] decrypted = XTSAES.decryptBlock(this.key,(i+16)/16,block);
+					byte[] decrypted = XTSAES.decryptBlock(this.key,0,block);
 					int c = 0;
 					for(int j = len-diff; j < len; j++)	{
 						dec[j] = decrypted[c++];
@@ -103,13 +104,13 @@ public class Xts{
 					}
 					d = i/16;
 				} else	{
-					byte[] decrypted = XTSAES.decryptBlock(this.key,i,block);
+					byte[] decrypted = XTSAES.decryptBlock(this.key,0,block);
 					for(int j = i; j < i+16; j++)	{
 						dec[j] = decrypted[j%16];
 					}
 				}
 			}
-			byte[] final_block = XTSAES.decryptBlock(this.key,d,last);
+			byte[] final_block = XTSAES.decryptBlock(this.key,0,last);
 			int last_empty_block = len-diff-16;
 			for(int i = 0; i < 16; i++)	{
 				dec[last_empty_block+i] = final_block[i];
